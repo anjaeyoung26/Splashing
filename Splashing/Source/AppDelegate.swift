199641b352088ee55fc1f92f418288bc3a6af5f7
Jaeyoung An <dkswodud011@naver.com>
2021-01-16 19:34:56 +0900

    Remove unused dispose bag

--- Splashing/Source/AppDelegate.swift
+++ Splashing/Source/AppDelegate.swift
@@ -6,24 +6,27 @@
 //  Copyright © 2020 안재영. All rights reserved.
 //
 
-import RxSwift
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    let disposeBag = DisposeBag()
-    
     var window: UIWindow?
     
-    private let splashCoordinator = splashCoordinator()
+    private var splashCoordinator: SplashCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = splashCoordinator.baseViewController
-        window?.backgroundColor = .white
-        window?.makeKeyAndVisible()
+        guard var _window = window else {
+            print("Could not load window.")
+            return false
+        }
+        
+        splashCoordinator = SplashCoordinator(window: _window)
+        
+        _window = UIWindow(frame: UIScreen.main.bounds)
+        _window.rootViewController = splashCoordinator?.baseViewController
+        _window.backgroundColor = .white
+        _window.makeKeyAndVisible()
         
         return true
     }
