24acb79c699ecc84f3a8b1a31257efd6bd07e088
Jaeyoung An <dkswodud011@naver.com>
2021-01-22 09:13:44 +0900

    Add scroll view parallax effect

---
+++ Splashing/Source/Util/Extension/Rx/UIScrollView+Rx.swift
@@ -0,0 +1,28 @@
+//
+//  UIScrollView+Rx.swift
+//  Splashing
+//
+//  Created by 안재영 on 2021/01/21.
+//  Copyright © 2021 안재영. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UIScrollView {
+    func parallex(with view: UIView) -> Binder<Void> {
+        return Binder(self.base) { scrollView, _ in
+            let yPos = scrollView.contentOffset.y
+            
+            if yPos < 0 {
+                let scale = 1 + ((-yPos) * 5 / view.frame.height)
+                view.transform = CGAffineTransform.identity
+                view.transform = CGAffineTransform(scaleX: scale, y: scale)
+                
+                var viewFrame = view.frame
+                viewFrame.origin.y = yPos
+                view.frame = viewFrame
+            }
+        }
+    }
+}
